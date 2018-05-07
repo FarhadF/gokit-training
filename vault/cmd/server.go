65d06313da06d82ca97bed95708509049f0edb5e
FarhadF <farhad.farahi@gmail.com>
2018-05-07 12:11:27 +0430

    fix flags and add console logging for zerolog

--- vault/cmd/server.go
+++ vault/cmd/server.go
@@ -13,21 +13,31 @@ import (
 	"net"
 	"gokit-training/vault/pb"
 	"google.golang.org/grpc"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	var (
-		httpAddr = flag.StringP("http", "H",":8080",
-			"http listen address")
-		gRPCAddr = flag.StringP("grpc", "G",":8081",
-			"gRPC listen address")
+		httpAddr string
+		gRPCAddr string
+		console bool
 	)
+	flag.StringVarP(&httpAddr, "http", "H",":8080","http listen address")
+	flag.StringVarP(&gRPCAddr, "grpc", "G",":8081","gRPC listen address")
+	flag.BoolVarP(&console, "console", "c", false, "turns on pretty console logging" )
 	flag.Parse()
 	ctx := context.Background()
 	//zerolog
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	//console pretty printing
+	if console {
+		logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+
+
 	svc := vault.NewService()
 	errChan := make(chan error)
+	//os signal handling
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
@@ -42,24 +52,24 @@ func main() {
 	}
 	// HTTP transport
 	go func() {
-		logger.Info().Str("http:", *httpAddr).Msg("")
+		logger.Info().Str("http:", httpAddr).Msg("")
 		handler := vault.NewHTTPServer(ctx, endpoints)
-		errChan <- http.ListenAndServe(*httpAddr, handler)
+		errChan <- http.ListenAndServe(httpAddr, handler)
 	}()
 	// GRPC transport
 	go func() {
-		listener, err := net.Listen("tcp", *gRPCAddr)
+		listener, err := net.Listen("tcp", gRPCAddr)
 		if err != nil {
 			errChan <- err
 			return
 		}
-		logger.Info().Str("grpc:", *gRPCAddr).Msg("")
+		logger.Info().Str("grpc:", gRPCAddr).Msg("")
 		handler := vault.NewGRPCServer(ctx, endpoints)
 		gRPCServer := grpc.NewServer()
 		pb.RegisterVaultServer(gRPCServer, handler)
 		errChan <- gRPCServer.Serve(listener)
 	}()
 
-	logger.Info().Msg(*gRPCAddr)
+	logger.Info().Msg(gRPCAddr)
 	logger.Fatal().Err(<-errChan).Msg("")
 }
