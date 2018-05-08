77a927ef4e2cc7bf01504d62ce5c2ba3afc83624
FarhadF <farhad.farahi@gmail.com>
2018-05-08 17:33:38 +0430

    added app logging middleware using zerolog in vault service

--- vault/cmd/server.go
+++ vault/cmd/server.go
@@ -56,6 +56,7 @@ func main() {
 	}, []string{}) // no fields here
 
 	svc := vault.NewService()
+	svc = vault.LoggingMiddleware{logger, svc}
 	svc = vault.InstrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 	errChan := make(chan error)
 	//os signal handling
