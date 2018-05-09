1d59a1880acb66ca73adac48f0f93f98b54d4f5c
FarhadF <farhad.farahi@gmail.com>
2018-05-09 09:20:30 +0430

    add applogging middleware for lorem

---
+++ lorem/logging.go
@@ -0,0 +1,28 @@
+package lorem
+import (
+	"github.com/rs/zerolog"
+	"time"
+	"context"
+)
+
+//struct passing the logger
+type LoggingMiddleware struct {
+	Logger zerolog.Logger
+	Next 	Service
+}
+
+//each method will have its own logger for app logs
+func (mw LoggingMiddleware)Lorem(ctx context.Context, requestType string, min int, max int) (output string, err error) {
+	defer func(begin time.Time) {
+		mw.Logger.Info().Str(
+			"method", "lorem").Str("requesttype", requestType).Int("min", min).Int("max",
+				max).Str("output", output).Err(err).Dur("took",
+			time.Since(begin)).Msg("")
+
+	}(time.Now())
+	output, err = mw.Next.Lorem(ctx, requestType, min, max)
+	return
+}
+
+
+
